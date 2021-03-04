d05d5f89b91c1eb01ad2bd364c29f5675f169f0d
Marcos Vinicius Inocencio <marcos.inocencio13@gmail.com>
2021-03-04 17:00:51 -0300

    App Navegação
    Testando navegação com React Native

---
+++ navegacao/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import { SafeAreaView, Text } from 'react-native'
+import TelaA from '../src/views/TelaA'
+import TelaB from '../src/views/TelaB'
+import TelaC from '../src/views/TelaC'
+
+export default props => (
+    <SafeAreaView style={{ flex: 1 }}>
+        <TelaA />
+        <TelaB />
+        <TelaC />
+    </SafeAreaView>
+)
