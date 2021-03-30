1ffd91d6d459579b02318d8aef729114e5711076
Marcos Vinicius Inocencio <marcos.inocencio13@gmail.com>
2021-03-30 17:15:37 -0300

    App Tasks
    App Tasks

---
+++ tasks/index.js
@@ -0,0 +1,9 @@
+/**
+ * @format
+ */
+
+import {AppRegistry} from 'react-native'
+import {name as appName} from './app.json'
+import Navigator from './src/Navigator'
+
+AppRegistry.registerComponent(appName, () => Navigator)
