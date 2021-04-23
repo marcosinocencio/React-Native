5d8127188386e824a59cdcb7fa30cce2051f064d
Marcos Vinicius Inocencio <marcos.inocencio13@gmail.com>
2021-04-23 16:34:57 -0300

    Uploading App
    App com funcionalidades básicas do Instagram

---
+++ lambe/index.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import { Provider } from 'react-redux'
+import { AppRegistry } from 'react-native';
+import App from './src/App'
+import { name as appName } from './app.json';
+
+import storeConfig from './src/store/storeConfig'
+import axios from 'axios'
+axios.defaults.baseURL = 'https://lambe-1acad-default-rtdb.firebaseio.com/'
+
+const store = storeConfig()
+const Redux = () => {
+    return (
+        <Provider store={store}>
+            <App />
+        </Provider>
+    )
+}
+
+AppRegistry.registerComponent(appName, () => Redux);
